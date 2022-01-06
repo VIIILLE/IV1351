08091c3091132c384392205685b8ac340ce4b06b
VIIILLE <93411785+VIIILLE@users.noreply.github.com>
2022-01-06 18:22:24 +0100

    Added commands for creating sample data

---
+++ exampleData.sql
@@ -0,0 +1,28 @@
+INSERT INTO person VALUES ('1','0105291337','Ville','Vik','20','Grindhagsvagen','19453','stockholm');
+INSERT INTO email VALUES ('1','villevik@gmail.com');
+INSERT INTO phone VALUES ('1','0701234567');
+INSERT INTO student VALUES ('1','1','vvik','1');
+INSERT INTO rent VALUES ('1','100','2022-01-01 12:00:00');
+INSERT INTO instrument_for_rent VALUES ('1','1','Electric guitar','Tenson','1');
+
+
+
+INSERT INTO person VALUES ('2','0205291337','Emil','Vik','23','Ronnvagen','19454','stockholm');
+INSERT INTO email VALUES ('2','emilvik@gmail.com');
+INSERT INTO phone VALUES ('2','0701234568');
+INSERT INTO student VALUES ('2','2','evik','0');
+
+INSERT INTO person VALUES ('3','6505291337','Soren','Vik','53','Grindhagsvagen','19453','stockholm');
+INSERT INTO email VALUES ('3','sorenvik@gmail.com');
+INSERT INTO phone VALUES('3','0701234569');
+
+
+INSERT INTO person VALUES ('4','7505291337','Karl','Svensson','43','Strandvagen','10053','stockholm');
+INSERT INTO email VALUES ('4','karl@gmail.com');
+INSERT INTO email VALUES ('4','karl@kth.com');
+INSERT INTO phone VALUES ('4','0701234560');
+INSERT INTO instructor VALUES ('1','ksve','yes','4');
+INSERT INTO instrument_knowledge VALUES ('1','Electric guitar');
+
+INSERT INTO lesson VALUES ('1','303','Beginner','1','1');
+INSERT INTO individual_lesson VALUES ('1','2022-01-02 13:00:00','Electric guitar');
